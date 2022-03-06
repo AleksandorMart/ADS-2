4a4f66a30516535aea878ec2254e24a654189a04
AleksandorMart <99171200+AleksandorMart@users.noreply.github.com>
2022-03-06 21:23:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,32 +4,32 @@
 
 
 double pown(double value, uint16_t n) {
- if (n == 0) return 1;
- return (value * pow(valur, n-1));
+if (n == 0) return 1;
+return (value * pow(valur, n-1));
 }
 
 uint64_t fact(uint16_t n) {
- if (n == 0) return 1;
- return (n * fact(n-1));
+if (n == 0) return 1;
+return (n * fact(n-1));
 }
 
 double calcItem(double x, uint16_t n) {
- return (pown(x, n) / fact(n));
+return (pown(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
- if (count == 0) return 1;
- return (calcItem(x, count) + expn(x, count-1));
+if (count == 0) return 1;
+return (calcItem(x, count) + expn(x, count-1));
 }
 
 double sinn(double x, uint16_t count) {
- if (count == 1) return x;
- if (count % 2 == 0) return (sinn(x, count-1) - calcItem(x, 2*count-1));
- if (count % 2 != 0) return (sinn(x, count-1) + calcItem(x, 2*count-1));
+if (count == 1) return x;
+if (count % 2 == 0) return (sinn(x, count-1) - calcItem(x, 2*count-1));
+if (count % 2 != 0) return (sinn(x, count-1) + calcItem(x, 2*count-1));
 }
 
 double cosn(double x, uint16_t count) {
- if (count == 1) return 1;
- if (count % 2 == 0) return (cosn(x, count-1) - calcItem(x, 2*count-2));
- if (count % 2 != 0) return (cosn(x, count-1) + calcItem(x, 2*count-2));
+if (count == 1) return 1;
+if (count % 2 == 0) return (cosn(x, count-1) - calcItem(x, 2*count-2));
+if (count % 2 != 0) return (cosn(x, count-1) + calcItem(x, 2*count-2));
 }
